ff939f8161726f5b5f7e3d41ce925efc0421e2e8
Jordan Anthony Rodak <jrodak@corn18.stanford.edu>
2014-05-27 19:14:59 -0700

    Current version on task 2 code. NDCG score around 0.75

--- src/cs276/pa4/Learning2Rank.java
+++ src/cs276/pa4/Learning2Rank.java
@@ -24,7 +24,7 @@ public class Learning2Rank {
  		if (task == 1) {
 			learner = new PointwiseLearner();
 		} else if (task == 2) {
-		  boolean isLinearKernel = true;
+		  boolean isLinearKernel = false;
 			learner = new PairwiseLearner(isLinearKernel);
 		} else if (task == 3) {
 			
--- src/cs276/pa4/PairwiseLearner.java
+++ src/cs276/pa4/PairwiseLearner.java
@@ -21,7 +21,6 @@ import weka.filters.unsupervised.attribute.Standardize;
 
 public class PairwiseLearner extends Learner {
   private LibSVM model;
-  private boolean linear = false;
   public PairwiseLearner(boolean isLinearKernel){
     try{
       model = new LibSVM();
@@ -30,7 +29,6 @@ public class PairwiseLearner extends Learner {
     }
     
     if(isLinearKernel){
-      linear = true;
       model.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_LINEAR, LibSVM.TAGS_KERNELTYPE));
     }
   }
@@ -49,25 +47,6 @@ public class PairwiseLearner extends Learner {
     }
   }
   
-  private double[] extractData(Query q, Document d1, Document d2, double rel1, double rel2, Map<String, Double> dfs) {
-	  //double relevance = (rel1 - rel1 > 0) ? 1.0 : -1.0;
-	  double[] result = {0.0, 0.0, 0.0, 0.0, 0.0};
-	  Map<String, Map<String, Double>> tfs1 = Util.getDocTermFreqs(d1, q);
-	  Map<String, Map<String, Double>> tfs2 = Util.getDocTermFreqs(d2, q);
-	  
-	  for (int i = 0; i < Util.TFTYPES.length; i++) {
-	  	Map<String, Double> tf1 = tfs1.get(Util.TFTYPES[i]);
-	  	Map<String, Double> tf2 = tfs2.get(Util.TFTYPES[i]);
-	  	
-	  	for (String term : q.words) {
-	  		double df = dfs.containsKey(term) ? dfs.get(term) + 1.0 : 1.0;
-	  		double idf = Math.log10((Util.totFiles + 1.0)/df);
-	  		if (tf1 != null && tf1.containsKey(term)) result[i] += tf1.get(term) * idf;
-	  		if (tf2 != null && tf2.containsKey(term)) result[i] -= tf2.get(term) * idf;
-	  	}
-	  }
-	  return result;
-  }
   
   private Instances standardize(Instances instances) throws Exception{
 	  Standardize filter = new Standardize();
@@ -89,6 +68,15 @@ public class PairwiseLearner extends Learner {
 	  }
 	  return result;
   }
+
+  private double[] difference(double[] vec1, double[]vec2) {
+  	double[] result = {0.0, 0.0, 0.0, 0.0, 0.0};
+  	for (int i = 0; i < result.length; i++) {
+  		result[i] += vec1[i];
+  		result[i] -= vec2[i];
+  	}
+  	return result;
+  }
   
 	@Override
 	public Instances extract_train_features(String train_data_file,
@@ -107,37 +95,75 @@ public class PairwiseLearner extends Learner {
 		attributes.add(new Attribute("body_w"));
 		attributes.add(new Attribute("header_w"));
 		attributes.add(new Attribute("anchor_w"));
+		
+		Instances toNormalize = new Instances("normalized", attributes, 0);
+
 		attributes.add(new Attribute("classification", classes));
 		dataset = new Instances("train_dataset", attributes, 0);
 		
 		/* Set last attribute as target */
 		dataset.setClassIndex(dataset.numAttributes() - 1);
 		
+		/* query -> docs */
 		Map<Query,List<Document>> queryDict = Util.loadTrainData(train_data_file);
 		/* query -> (url -> score) */
 		Map<String, Map<String, Double>> relevanceScores = Util.loadRelData(train_rel_file);
-		
+		/* query -> (url -> index) */
+		Map<String, Map<String, Integer>> instanceIndexes = new HashMap<String, Map<String, Integer>>();
+
+		int index = 0;
+		for (Entry<Query,List<Document>> entry : queryDict.entrySet()) {
+			Query q = entry.getKey();
+			List<Document> docs = entry.getValue();
+			Map<String, Integer> indexes = new HashMap<String, Integer>();
+			for (Document d : docs) {
+				double[] tfvector = getTfVector(q, d, idfs);
+				Instance inst = new DenseInstance(1.0, tfvector);
+				toNormalize.add(inst);
+				indexes.put(d.url.toString(), index++);
+			}
+			instanceIndexes.put(q.toString(), indexes);
+		}
+
+		Instances normalized = standardize(toNormalize);
+
+		int count = 0;
 		for (Entry<Query, List<Document>> entry : queryDict.entrySet()) {
 			Query q = entry.getKey();
 			List<Document> docs = entry.getValue();
 			for (int i = 0; i < docs.size(); i++) {
 				Document d1 = docs.get(i);
 				double rel1 = relevanceScores.get(q.toString()).get(d1.url.toString()); 
-				for (int j = i; j < docs.size(); j++) {
+				for (int j = i + 1; j < docs.size(); j++) {
 					Document d2 = docs.get(j);
 					double rel2 = relevanceScores.get(q.toString()).get(d2.url.toString()); 
-					double[] instance = extractData(q, d1, d2, rel1, rel2, idfs);
-					Instance inst = new DenseInstance(1.0, instance);
-					String dataclass = (rel1 > rel2) ? "1" : "-1";
+					double[] tfvector1 = normalized.get(instanceIndexes.get(q.toString()).get(d1.url.toString())).toDoubleArray();
+					double[] tfvector2 = normalized.get(instanceIndexes.get(q.toString()).get(d2.url.toString())).toDoubleArray();
+
+					Instance inst = null;
+					String dataclass;
+					if ((((count % 2 == 0)) && (rel1 > rel2)) || ((count % 2 != 0) && (rel1 < rel2))) {
+
+						double[] instances = difference(tfvector1, tfvector2);
+						inst = new DenseInstance(1.0, instances);
+					} else {
+						double[] instances = difference(tfvector2, tfvector1);
+						inst = new DenseInstance(1.0, instances);
+					}
+
+					dataclass = (count % 2 == 0) ? "1" : "-1";
+
+
 					inst.insertAttributeAt(inst.numAttributes());
 					inst.setDataset(dataset);
 					inst.setClassValue(dataclass);
 					dataset.add(inst);
+					count++;
 				}
 			}
 		}
 		
-		return standardize(dataset);
+		return dataset;
 	}
 
 	@Override
@@ -180,29 +206,13 @@ public class PairwiseLearner extends Learner {
 			testFeatures.index_map.put(q.query, new HashMap<String,Integer>());
 			List<Document> docs = entry.getValue();
 			
-//			for (int i = 0; i < docs.size(); i++) {
-//				Document d1 = docs.get(i);
-//				for (int j = i; j < docs.size(); j++) {
-//					Document d2 = docs.get(j);
-//					double[] instance = extractData(q, d1, d2, 0.0, 0.0, idfs);
-//					Instance inst = new DenseInstance(1.0, instance);
-//					inst.insertAttributeAt(inst.numAttributes());
-//					inst.setDataset(features);
-//					features.add(inst);
-//					
-//					testFeatures.index_map.get(q.query).put(d1.url + "," + d2.url, index);
-//					index++;	
-//				}
-//			}
-			
 			for (Document d : docs) {
 				double[] instance = getTfVector(q, d, idfs);
 				Instance inst = new DenseInstance(1.0, instance);
 				inst.insertAttributeAt(inst.numAttributes());
 				inst.setDataset(features);
 				features.add(inst);
-				testFeatures.index_map.get(q.query).put(d.url.toString(), index);
-				index++;
+				testFeatures.index_map.get(q.query).put(d.url.toString(), index++);
 			}
 		}
 		
@@ -212,35 +222,39 @@ public class PairwiseLearner extends Learner {
 	}
 
 	
-	private double linearScore(TestFeatures tf, Classifier model, String q, String str) {
-		double[] weights = ((LibSVM)model).coefficients();
-		double[] x = tf.features.get(tf.index_map.get(q).get(str)).toDoubleArray();
-		double score = 0.0;
-		for (int i = 0; i < x.length; i++) {
-			score += weights[i] * x[i];
+	private int compareDocs(TestFeatures tf, Classifier model, String q, String url1, String url2)  {
+		double[] tfvector1 = tf.features.get(tf.index_map.get(q).get(url1)).toDoubleArray();
+		double[] tfvector2 = tf.features.get(tf.index_map.get(q).get(url2)).toDoubleArray();
+		double[] difference = difference(tfvector1, tfvector2);
+		Instance inst = new DenseInstance(1.0, difference);
+		inst.setDataset(tf.features);
+		double classification = 1.0;
+		try {
+			classification = model.classifyInstance(inst);
+			//System.out.println(classification);
+		} catch (Exception e) {
+			System.err.println("An error occured while classifying.");
 		}
-		return score;
+		return (classification > 0.0) ? -1 : 1;
 	}
-	
+
 	@Override
 	public Map<String, List<String>> testing(final TestFeatures tf,
-			final Classifier model) {
+		final Classifier model) throws Exception{
 		Map<String, List<String>> allRankings = new HashMap<String, List<String>>();
 		for (Entry<String, Map<String, Integer>> entry : tf.index_map.entrySet()) {
 			final String q = entry.getKey();
 			List<String> rankings = new ArrayList<String>();
-			rankings.addAll(entry.getValue().keySet());
-			if (linear) {
-				Collections.sort(rankings, new Comparator<String>() {
-					@Override
-					public int compare(String str1, String str2) 
-					{
-						Double score1 = linearScore(tf, model, q, str1);
-						Double score2 = linearScore(tf, model, q, str2);
-						return score2.compareTo(score1);
-					}	
-				});
+			for (String url : entry.getValue().keySet()) {
+				rankings.add(url);
 			}
+			Collections.sort(rankings, new Comparator<String>() {
+				@Override
+				public int compare(String url1, String url2)
+				{
+					return compareDocs(tf, model, q, url1, url2);
+				}	
+			});
 			allRankings.put(q, rankings);
 		}
 		return allRankings;
